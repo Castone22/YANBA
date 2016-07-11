088cbc8e8e8871713233d43e54a1ee6e4cc16663
luke ridge <castone22@gmail.com>
2016-07-11 12:19:36 -0400

    Added the first page object

---
+++ YANBA/YANBA.Spec/PageObjects/IndexPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YANBA.Spec.PageObjects
+{
+    class IndexPage
+    {
+        private readonly IWebDriver driver;
+        private readonly string url = @"http://localhost:51991/";
+
+
+        public IndexPage(IWebDriver browser)
+        {
+            this.driver = browser;
+            PageFactory.InitElements(browser, this);
+        }
+    }
+}
