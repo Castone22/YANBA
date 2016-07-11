fb0cd9898b48c789b741734fbcfb15d942251558
luke ridge <castone22@gmail.com>
2016-07-11 14:41:16 -0400

    Added elements to post/index pages.

--- YANBA/YANBA.Spec/PageObjects/IndexPage.cs
+++ YANBA/YANBA.Spec/PageObjects/IndexPage.cs
@@ -19,5 +19,41 @@ namespace YANBA.Spec.PageObjects
             this.driver = browser;
             PageFactory.InitElements(browser, this);
         }
+
+
+        [FindsBy(How = How.Id, Using = "login_form_user_box")]
+        public IWebElement LoginFormUserBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "login_form_passsword_box")]
+        public IWebElement LoginFormPasswordBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "login_form_submit_button")]
+        public IWebElement LoginFormSubmitButton { get; set; }
+
+
+
+        [FindsBy(How = How.Id, Using = "search_text_box")]
+        public IWebElement SearchTextBox { get; set; }
+
+        [FindsBy(How = How.Id, Using = "search_submit_button")]
+        public IWebElement SearchSubmitButton { get; set; }
+
+
+
+        [FindsBy(How = How.Id, Using = "post_summary_text_")]
+        public IWebElement PostSummaryText { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_link_")]
+        public IWebElement PostList { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_author_")]
+        public IWebElement PostAuthor { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_date_")]
+        public IWebElement PostDate { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_title_")]
+        public IWebElement PostTitle { get; set; }
+
     }
 }
---
+++ YANBA/YANBA.Spec/PageObjects/PostPage.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YANBA.Spec.PageObjects
+{
+    class PostPage
+    {
+        private readonly IWebDriver driver;
+
+
+        public PostPage(IWebDriver browser)
+        {
+            this.driver = browser;
+            PageFactory.InitElements(browser, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "post_content")]
+        public IWebElement PostContent { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_author")]
+        public IWebElement PostAuthor { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_date")]
+        public IWebElement PostDate { get; set; }
+
+        [FindsBy(How = How.Id, Using = "post_title")]
+        public IWebElement PostTitle { get; set; }
+
+    }
+}
