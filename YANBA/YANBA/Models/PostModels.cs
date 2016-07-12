ab48f3406fa51707512e04058c633e6ff0fe9af5
Martin Holmes <holmesmrtn@aol.com>
2016-07-12 11:02:53 -0400

    Started creation of Post model class

---
+++ YANBA/YANBA/Models/PostModels.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace YANBA.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class PostDBContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+    }
+}
